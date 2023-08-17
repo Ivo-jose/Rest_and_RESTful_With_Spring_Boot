7d4517b4750b5f4af54c0a5d2f224f09a13e2883
Ivo Goncalves <ivojmgs19@gmail.com>
2023-08-17 11:05:02 -0300

    Migrating to Postgres

---
+++ Rest_and_RESTful_With_Spring_Boot_Postgres/rest_with_spring_boot_and_java/src/main/java/br/com/ivogoncalves/data/vo/v1/PersonVO.java
@@ -0,0 +1,106 @@
+package br.com.ivogoncalves.data.vo.v1;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
+@JsonPropertyOrder({"id","firstName","lastName","address","gender","enabled"})
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Mapping("id")
+	@JsonProperty("id")
+	private Long idPerson;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String gender;
+	private Boolean enabled;
+	
+	public PersonVO() {}
+
+	public PersonVO(Long idPerson, String firstName, String lastName, String address, String gender, Boolean enabled) {
+		this.idPerson = idPerson;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.gender = gender;
+		this.enabled = enabled;
+	}
+
+	public Long getIdPerson() {
+		return idPerson;
+	}
+
+	public void setIdPerson(Long idPerson) {
+		this.idPerson = idPerson;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(address, enabled, firstName, gender, idPerson, lastName);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonVO other = (PersonVO) obj;
+		return Objects.equals(address, other.address) && Objects.equals(enabled, other.enabled)
+				&& Objects.equals(firstName, other.firstName) && Objects.equals(gender, other.gender)
+				&& Objects.equals(idPerson, other.idPerson) && Objects.equals(lastName, other.lastName);
+	}
+}
---
+++ Rest_and_RESTful_With_Spring_Boot_Postgres/rest_with_spring_boot_and_java/src/test/java/br/com/ivogoncalves/unittests/mockito/services/BookServicesTest.java
@@ -0,0 +1,116 @@
+package br.com.ivogoncalves.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.ivogoncalves.data.vo.v1.BookVO;
+import br.com.ivogoncalves.exceptions.RequiredObjectIsNullException;
+import br.com.ivogoncalves.model.Book;
+import br.com.ivogoncalves.respositories.BookRepository;
+import br.com.ivogoncalves.services.BookServices;
+import br.com.ivogoncalves.unittests.mapper.mocks.MockBook;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class BookServicesTest {
+	
+	MockBook input;
+	
+	@InjectMocks
+	private BookServices service;
+	@Mock
+	BookRepository repository;
+	
+	@BeforeEach
+	void setUpMocks() throws Exception {
+		input = new MockBook();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testFindById() {
+		Book entity = input.mockEntity(1);
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		var result = service.findById(1L);
+		assertNotNull(result);
+		assertNotNull(result.getIdBook());
+		assertNotNull(result.getLinks());
+		assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+		assertEquals("Title Test1", result.getTitle());
+		assertEquals("Author Test1", result.getAuthor());
+		assertNotNull(result.getLaunchDate());
+		assertEquals(101.0, result.getPrice());
+	}
+
+	@Test
+	void testCreate() {
+		Book entity = input.mockEntity(1);;
+		Book persisted = entity;
+		BookVO vo = input.mockVO(1);
+		when(repository.save(entity)).thenReturn(persisted);
+		var result = service.create(vo);
+		assertNotNull(result);
+		assertNotNull(result.getIdBook());
+		assertNotNull(result.getLinks());
+		assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+		assertEquals("Title Test1", result.getTitle());
+		assertEquals("Author Test1", result.getAuthor());
+		assertNotNull(result.getLaunchDate());
+		assertEquals(101.0, result.getPrice());
+	}
+	
+	
+	@Test
+	void testCreateWithNullBook() {
+		Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {service.create(null);});
+		String expectedMessage = "Not allowed to persist a null object";
+		assertTrue(exception.getMessage().contains(expectedMessage));
+	}
+
+	@Test
+	void testUpdate() {
+		Book entity = input.mockEntity(1);;
+		Book persisted = entity;
+		BookVO vo = input.mockVO(1);
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		when(repository.save(entity)).thenReturn(persisted);
+		var result = service.update(vo);
+		assertNotNull(result);
+		assertNotNull(result.getIdBook());
+		assertNotNull(result.getLinks());
+		assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
+		assertEquals("Title Test1", result.getTitle());
+		assertEquals("Author Test1", result.getAuthor());
+		assertNotNull(result.getLaunchDate());
+		assertEquals(101.0, result.getPrice());
+	}
+	
+	@Test
+	void testUpdateWithNullBook() {
+		Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {service.update(null);});
+		String expectedMessage = "Not allowed to persist a null object";
+		assertTrue(exception.getMessage().contains(expectedMessage));
+	}
+
+	@Test
+	void testDelete() {
+		Book entity = input.mockEntity(1);
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		service.delete(1L);
+	}
+}
