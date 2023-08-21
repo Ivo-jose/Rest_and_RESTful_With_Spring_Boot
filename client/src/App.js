1850a66e4053462a9bc5cdfaea350e8be4b6e307
Ivo Goncalves <ivojmgs19@gmail.com>
2023-08-21 17:21:17 -0300

    Created Header Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+//Components
+import Header from './Header';
+
 export default function App() {
   return (
-    <h1>Hello world!</h1>
+   <Header>
+      Hello Client Udemy - Properties
+   </Header>
   );
 }
 
