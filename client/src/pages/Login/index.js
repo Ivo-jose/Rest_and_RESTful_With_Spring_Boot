12722df2656a0bc98c5a81e594b121a62ec0e988
Ivo Goncalves <ivojmgs19@gmail.com>
2023-08-23 15:52:20 -0300

    HTML and CSS Component Login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+//CSS
 import './styles.css'
+//Images
+import logo from '../../assets/logo.svg'
+import padLock from '../../assets/padlock.png';
 
 export default function Login() {
     return(
-        <h1>Welcome</h1>
+        <div className='login-container'>
+            <section className="form">
+               <img src={logo} alt="Logo" /> 
+               <form action="">
+                  <h1>Access your account</h1>
+                  <input type="text" placeholder='Username'/>  
+                  <input type="text" placeholder='Password'/>  
+                  <button className='button' type='submit'>Login</button>
+               </form>
+            </section>
+            <img src={padLock} alt="padlock"/>
+        </div>
     );
 }
