ca3d0d3359ba2db50e6739dabc77bd4f7fec9576
Ivo Goncalves <ivojmgs19@gmail.com>
2023-08-24 10:01:32 -0300

    Created Route to Book Component

---
+++ client/src/pages/Book/index.js
@@ -0,0 +1,10 @@
+//Libs
+import React from "react";
+//CSS
+import './styles.css';
+
+export default function Book() {
+    return(
+        <h1>Books</h1>
+    );
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Components
 import Login from './pages/Login/index';
+import Book from './pages/Book';
 
 export default function AppRoutes() {
     return(
          <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <Login/> } />
+                <Route path="/book" element={ <Book/> } />
             </Routes>
          </BrowserRouter>    
     );
